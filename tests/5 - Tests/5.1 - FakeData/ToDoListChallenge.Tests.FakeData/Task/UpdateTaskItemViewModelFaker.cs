0661d84e855b5f41b9bd699e09b1bf0ecf05d212
Lucas Sobrinho <lucas.ns.93@hotmail.com>
2026-05-04 08:31:18 -0300

    fix: updater fake data

--- tests/5 - Tests/5.1 - FakeData/ToDoListChallenge.Tests.FakeData/Task/UpdateTaskItemViewModelFaker.cs
+++ tests/5 - Tests/5.1 - FakeData/ToDoListChallenge.Tests.FakeData/Task/UpdateTaskItemViewModelFaker.cs
@@ -9,10 +9,7 @@ namespace ToDoListChallenge.Tests.FakeData.Task
         public UpdateTaskItemViewModelFaker()
         {
             RuleFor(t => t.Id, f => f.Random.Guid());
-            RuleFor(t => t.Title, f => f.Lorem.Sentence(3));
-            RuleFor(t => t.Description, f => f.Lorem.Paragraph());
             RuleFor(t => t.Status, f => f.PickRandom<TaskItemStatus>());
-            RuleFor(t => t.DueDate, f => DateOnly.FromDateTime(f.Date.Future()));
         }
     }
 }
